700c9e63f2b58a8187fda7fdf1280cc7e1e676ae
Jonathan Cardoz <jcardoz@deloitte.com>
2021-10-31 23:44:12 +0530

    fix: formatting

--- restaurant-react/src/components/Footer/index.js
+++ restaurant-react/src/components/Footer/index.js
@@ -3,11 +3,11 @@ import './style.css'
 
 const Footer = () => {
     return (
-        <div class="footer">
-            <h3>
-                <a href="mailto:vyb@something.com">vyb@something.com</a> | <a href="twitter.com/vyb">Twitter</a> | <a href="whatsapp.com/vyb">WhatsApp</a>
-            </h3>
-        </div>
+        <footer>
+            <a href="mailto:vyb@something.com">vyb@something.com</a> | 
+            <a href="twitter.com/vyb">Twitter</a> | 
+            <a href="whatsapp.com/vyb">WhatsApp</a>
+        </footer>
     )
 }
 
