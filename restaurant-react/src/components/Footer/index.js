73ace680410326e7f9c864faab28888beec17f43
Jonathan Cardoz <jcardoz@deloitte.com>
2021-10-31 23:41:57 +0530

    add(Footer): add footer links and formatting

--- restaurant-react/src/components/Footer/index.js
+++ restaurant-react/src/components/Footer/index.js
@@ -3,8 +3,10 @@ import './style.css'
 
 const Footer = () => {
     return (
-        <div>
-
+        <div class="footer">
+            <h3>
+                <a href="mailto:vyb@something.com">vyb@something.com</a> | <a href="twitter.com/vyb">Twitter</a> | <a href="whatsapp.com/vyb">WhatsApp</a>
+            </h3>
         </div>
     )
 }
